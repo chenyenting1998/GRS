180df3b0236288deb92fe9920f83c7473c1a2027
chenyenting1998 <tumhark@gmail.com>
2023-07-08 18:03:01 +0800

    update analysis_env

--- analysis/analysis_env.R
+++ analysis/analysis_env.R
@@ -25,137 +25,88 @@ library(ggrepel)
 library(writexl) # xlsx
 library(GRSmacrofauna)  # data package
 
-
 # load variables
 load("data/cruise_color.RData")
 load("data/env_variables.RData")
+load("data/env_long.RData")
 
 ################################
-# 1. Convert env to long format
+# 1. Select interested variables
 ################################
-# env long
-env_long <- 
-  env %>% 
-  filter(!Station %in% c("GC1", "GS1")) %>% 
-  pivot_longer(cols = all_of(env_variables),
-               names_to = "Variables",
-               values_to = "Values")
+# omit variables
+omit <- c("Salinity", "Density", "SigmaTheta", "Oxygen", "Transmission",
+          "Clay", "Silt", "Sand",
+          "delta13C",
+          "TN",
+          "WC")
+env_spatialvariables <- c("Depth", "DRM")
+# Reasoning:
+#  Salinity: salinity were within the open water range, and snapshots 
+#            of salinity change cannot inform us the variability of salinity.
+#  Density: not known to be an important factor.
+#  Transmission: strong correlation with temperature
+#  Oxygen: bottom water is well oxygenated.
+#  clay, silt, sand: D50 was used as the sole granulometry proxy 
+#  Delta13C: strong correlation with Chla; mixed signals based on geological research
+#  TN: strong collinearity with porosity.
+#  Water content: strong collinearity with porosity.
 
-#####################
-# 2. Data exploration
-#####################
-# qqplot
-env_qqplot <-
-  ggplot(env_long, aes(sample = Values))+
-  geom_qq()+
-  geom_qq_line()+
-  facet_wrap(~Variables, scales = "free") +
-  scale_color_manual(values = cruise_color) +
-  theme_bw()
-  
-ggsave("figure/env_qqplot.png", scale = 1.5, plot = env_qqplot)
 
-# Correlation matrix
-png(filename = "figure/env_corr_all.png",
-    width = 1600,
-    height = 800)
+# subset env
+env_variables_selected <- colnames(env)[!colnames(env) %in% c(env_metadata, env_spatialvariables, omit)]
+env_selected <- env[,env_variables_selected]
 
-env_corr <- Hmisc::rcorr(as.matrix(env[,env_variables]))
-corrplot::corrplot(env_corr$r,
-                   # corrplot style
-                   method = "ellipse",
-                   type = "upper",
-                   diag = FALSE,
-                   addCoef.col = TRUE,
-                   # significance
-                   p.mat = env_corr$P,
-                   insig = "blank")
-dev.off()
+#############
+# 2. Pairplot
+#############
+env_pairplot <-
+  env_selected %>% 
+  ggpairs()
 
-# boxplot
-env_boxplot <-
-  env_long %>% 
-  filter(!Variables %in% c("Sigma-Theta", "WC")) %>% 
-  ggplot(aes(x = Cruise, y = Values, color = Cruise))+
-  geom_boxplot(outlier.shape = NA) +
-  geom_point(position = "jitter") +
-  facet_wrap( ~ factor(Variables, env_variables), 
-             scales = "free") +
-             # labeller = as_labeller(env_var_fn, label_parsed)) +
-  scale_color_manual(values = cruise_color) +
-  theme_bw() +
-  theme(legend.position = c(0.91, 0.1),
-        axis.text.x = element_text(size = 8),
-        strip.text = element_text(size = 8))
+ggsave("figure/polished/env_pairplot.png", scale = 1.4, plot = env_pairplot)
 
-ggsave("figure/env_boxplot.png", 
-       plot = env_boxplot,
-       width = 8,
-       height = 6,
-       scale = 1.2)
-
-###################
-# 3. Kruskal-Wallis 
-###################
+###########################
+# 3. Wilcoxon rank-sum test 
+###########################
 # set up an empty object
-kruskal_table <- NULL
-?t_test
-x <- 
+wilcox_table <- 
   env_long %>% 
   group_by(Variables) %>% 
   wilcox_test(Values ~ Cruise,
               comparisons = list(c("OR1-1219", "OR1-1242")))
-View(x)
-# for-loop kruskal.test() to all the variables
-for(variable in env_variables){
-  # write a formula for kruskal.test()
-  kruskal_formula <- as.formula(paste0(variable,"~", "Cruise"))
-  
-  # run test
-  output <- kruskal.test(kruskal_formula, data = env)
-  
-  # extract test result
-  result <- 
-    data.frame("Variable" = gsub(" .*", "", output$data.name),
-               "df" = output$parameter,
-               "Chi-squared" = output$statistic,
-               "p value" = output$p.value,
-               row.names = which(env_variables == variable))
-
-  # stitch the result with the previous ones
-  kruskal_table <- rbind(kruskal_table, result)
-}
-
 # output
-write_xlsx(kruskal_table, "table/env_kruskal_table.xlsx")
+write_xlsx(wilcox_table, "table/env_wilcox_table.xlsx")
 
 ###########################
-# 6. PERMANOVA and PERMDISP
+# 4. PERMANOVA and PERMDISP
 ###########################
 set.seed(10)
 # run permanova
 env_permanova <- 
-  adonis2(scale(env[,colnames(env) %in% env_variables]) ~ Cruise,
+  adonis2(scale(env_selected) ~ Cruise,
   # adonis2(scale(env_selected) ~ Cruise,
           data = env, 
           method = "euclidean",
-          permutations = 9999)
+          permutations = 99999)
 # run permdisp
 env_disp <-
-  betadisper(vegdist(env[, colnames(env) %in% env_variables],
+  betadisper(vegdist(env_selected,
                      method = "euclidean", scale = TRUE), 
              group = env$Cruise)
 # env_disp <- betadisper(vegdist(env_selected, method = "euclidean", scale = TRUE), group = env$Cruise)
-env_permdisp <- permutest(env_disp, permutations = 9999)
+env_permdisp <- permutest(env_disp, permutations = 99999)
 
-# check output
-env_permanova
-env_permdisp
+# output
+permanova_output <-
+  list(PERMANOVA = as.data.frame(env_permanova),
+       PERMDISP = as.data.frame(env_permdisp$tab))
+write_xlsx(permanova_output,
+           "table/env_permanova.xlsx")
 
 ##################################
 # 5. Principle coordinate analysis
 ##################################
-env_pca <- rda(scale(env[, colnames(env) %in% env_variables]))
+env_pca <- rda(scale(env_selected))
 
 # extract sites
 env_pca_sites <- 
@@ -204,50 +155,15 @@ env_pca_plot <-
   theme_bw() +
   theme(legend.position = c(0.1,0.1))
 
-ggsave("figure/env_pca_plot.png", 
+ggsave("figure/polished/env_pca_plot.png", 
        plot = env_pca_plot,
        width = 8,
        height = 6,
        scale = 1)
 
-###############################
-# 6. Select interested variables
-###############################
-# omit variables
-omit <- c("Salinity", "Density", "SigmaTheta", "Oxygen",
-          "Clay", "Silt", "Sand", "WC")
-omit_space <- c("Depth", "DRM")
-# Reasoning:
-#  Salinity: snapshots of salinity change cannot inform us the variability of salinity.
-#  Density: might not be an important factor
-#  Oxygen: bottom water is well oxygenated.
-#  clay, silt, sand: D50 was used as the sole granulometry proxy 
-#  Water content: strong collinearity with porosity.
-
-# subset env
-env_selected <- env[,!colnames(env) %in% c(env_metadata, omit_space, omit)]
-
-
-# omit some clearly correlated variables
-corrplot::corrplot(cor(env[,env_var[!env_var %in% c(omit, omit_space)]]),
-                   addCoef.col = TRUE)
-# pair plot (with reduced variables)
-pairplot_ggplot <-
-  env %>% 
-  ggpairs(columns = c(env_var[!env_var %in% c(omit)]),
-          legend = 1,
-          aes(color = Cruise, fill = Cruise)) +
-  scale_color_manual(values = cruise_color) +
-  scale_fill_manual(values = cruise_color) +
-  theme_bw()
-
-ggsave("figure/env_pairs.png", 
-       pairplot_ggplot, 
-       width = 16,
-       height = 9,
-       scale = 1.2)
-
-# Depth and DRM are dropped since I am interested in analyzing 
-#   the relationship between in situ environment and the biota.
-
-save(env_selected, file = "data/env_sel.RData")
+###########
+# 6. Output
+###########
+save(env_variables_selected, 
+     env_selected, 
+     file = "data/env_selected.RData")
