6da276fbd339f322b3eb38ce5db8c98c31d02c7b
chenyenting1998 <tumhark@gmail.com>
2023-07-10 16:07:58 +0800

    update macrofauna composition code

--- analysis/analysis_macrofauna composition.R
+++ analysis/analysis_macrofauna composition.R
@@ -31,8 +31,8 @@ load("data/env_variables.Rdata")
 load("data/wide_data.Rdata")
 
 # Load functions
-log10_hell_dist <- function(data){
-  dist<- 
+log10_hell <- function(data){
+  hell <- 
     data %>% 
     ungroup() %>%
     select(-Cruise, -Station, -Deployment, -Tube) %>% 
@@ -40,7 +40,7 @@ log10_hell_dist <- function(data){
     decostand(method = "log", logbase = 10) %>%
     # Hellinger transformation to deal with double zero
     decostand(method = "hellinger") 
-  return(dist)
+  return(hell)
 }
 
 # extract pca data for plotting
@@ -94,7 +94,7 @@ plot_scree <-
   }
 
 plot_pca <-
-  function(sites, species, eig_vector, scaling){
+  function(sites, species, eig_vector, scaling, stretch){
   if(scaling == 1){
     plot <-
       ggplot()+
@@ -136,10 +136,11 @@ plot_pca <-
       
       # plot sp.
       geom_segment(data = species[species$Show == TRUE,],
-                   aes(x = 0, y = 0, xend = PC1, yend = PC2),
+                   aes(x = 0, y = 0, xend = PC1 * stretch, yend = PC2 * stretch),
                    size = .4, color = "black")+
       geom_label(data = species[species$Show == TRUE,],
-                 aes(x = PC1, y = PC2, label = Taxon)) +
+                 aes(x = PC1 * stretch, y = PC2 * stretch, label = Taxon),
+                 alpha = 0.5) +
       
       # change axis label
       xlab(paste0("PC1 (", eig_vector[1], "% of total variance explained)")) +
@@ -156,8 +157,8 @@ plot_pca <-
 # 1. Principal component analysis -- Count data
 ###############################################
 # Plot PCA with count data 
-count_dist <- log10_hell_dist(count_wide) 
-count_pca <- rda(count_dist)
+count_hell <- log10_hell(count_wide) 
+count_pca <- rda(count_hell)
 
 # get output
 count_sc1 <- get_pca_output(count_pca, metadata = count_wide[1:2], scaling = 1)
@@ -166,18 +167,18 @@ count_sc2 <- get_pca_output(count_pca, metadata = count_wide[1:2], scaling = 2)
 count_scree <- plot_scree(count_pca)
 # plot pca plots
 count_pca_sc1 <- plot_pca(count_sc1$pca_sites, count_sc1$pca_species, scaling = 1, eig_vector = count_sc1$pca_eig)
-count_pca_sc2 <- plot_pca(count_sc2$pca_sites, count_sc2$pca_species, scaling = 2, eig_vector = count_sc2$pca_eig)
+count_pca_sc2 <- plot_pca(count_sc2$pca_sites, count_sc2$pca_species, scaling = 2, eig_vector = count_sc2$pca_eig, stretch = 2)
 
 # output
-ggsave(filename = "figure/polished/count_pca_sc1.png", plot = count_pca_sc1)
-ggsave(filename = "figure/polished/count_pca_sc2.png", plot = count_pca_sc2)
+ggsave(filename = "figure/polished/count_pca_sc1.png", plot = count_pca_sc1, scale = 1.5)
+ggsave(filename = "figure/polished/count_pca_sc2.png", plot = count_pca_sc2, scale = 1.5)
 
-###############################################
+#################################################
 # 2. Principal component analysis -- Biomass data
-###############################################
+#################################################
 # Plot PCA with count data 
-biomass_dist <- log10_hell_dist(biomass_wide) 
-biomass_pca <- rda(biomass_dist)
+biomass_hell <- log10_hell(biomass_wide) 
+biomass_pca <- rda(biomass_hell)
 
 # get output
 biomass_sc1 <- get_pca_output(biomass_pca, metadata = biomass_wide[1:2], scaling = 1)
@@ -186,8 +187,48 @@ biomass_sc2 <- get_pca_output(biomass_pca, metadata = biomass_wide[1:2], scaling
 count_scree <- plot_scree(count_pca)
 # plot pca plots
 biomass_pca_sc1 <- plot_pca(biomass_sc1$pca_sites, biomass_sc1$pca_species, scaling = 1, eig_vector = biomass_sc1$pca_eig)
-biomass_pca_sc2 <- plot_pca(biomass_sc2$pca_sites, biomass_sc2$pca_species, scaling = 2, eig_vector = biomass_sc2$pca_eig)
+biomass_pca_sc2 <- plot_pca(biomass_sc2$pca_sites, biomass_sc2$pca_species, scaling = 2, eig_vector = biomass_sc2$pca_eig, stretch = 2)
 
 # output
-ggsave(filename = "figure/polished/biomass_pca_sc1.png", plot = biomass_pca_sc1)
-ggsave(filename = "figure/polished/biomass_pca_sc2.png", plot = biomass_pca_sc2)
+ggsave(filename = "figure/polished/biomass_pca_sc1.png", plot = biomass_pca_sc1, scale = 1.5)
+ggsave(filename = "figure/polished/biomass_pca_sc2.png", plot = biomass_pca_sc2, scale = 1.5)
+
+
+####################################
+# 3. PERMANOVA and PERMDISP -- Count
+####################################
+# count
+set.seed(100)
+count_permanova <- 
+  adonis2(count_hell ~ Cruise + Station,
+          data = count_wide[,c("Cruise", "Station")],
+          method = "euclidean",
+          permutations = 99999)
+count_disp <- 
+  betadisper(vegdist(count_hell, method = "euclidean"),
+             group = count_wide$Cruise)
+count_permdisp <- 
+  permutest(count_disp, nperm = 99999)
+
+write_xlsx(list(PERMANOVA = as.data.frame(count_permanova),
+                PERMDISP = as.data.frame(count_permdisp$tab)),
+           path = "table/count_permanova.xlsx")
+
+# biomass
+set.seed(51)
+biomass_permanova <- 
+  adonis2(biomass_hell ~ Cruise,
+          data = biomass_wide[,c("Cruise", "Station")],
+          method = "euclidean",
+          permutations = 99999)
+biomass_disp <- 
+  betadisper(vegdist(biomass_hell, method = "euclidean"),
+             group = biomass_wide$Cruise)
+biomass_permdisp <- 
+  permutest(biomass_disp, nperm = 99999)
+
+write_xlsx(list(PERMANOVA = as.data.frame(biomass_permanova),
+                PERMDISP = as.data.frame(biomass_permdisp$tab)),
+           path = "table/biomass_permanova.xlsx")
+
+
